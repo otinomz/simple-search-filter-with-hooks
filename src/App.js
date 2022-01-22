75fc3e0599de687db4defb19ebca415ceb412eef
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-22 02:55:26 +0100

    feat: setting up the hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
+import React from "react";
 
-export default function App() {
+export default function App({ items }) {
+  const [filteredItems, setFilteredItems] = React.useState(items);
+
   return (
     <div className="App">
       <h1>React Simple Search filter using hooks</h1>
-      
     </div>
   );
 }
